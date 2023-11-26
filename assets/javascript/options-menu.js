5ca23702d4cdf1a06177b242fdfa38d86848db7f
Picasso <urvishp13@gmail.com>
2023-11-26 17:57:45 -0500

    got dragging and dropping of exercises to work

--- assets/javascript/options-menu.js
+++ assets/javascript/options-menu.js
@@ -5,7 +5,7 @@ const exercisesList = document.getElementById("exercises-list")
 // add a click event to each of the ellipsis to function as a more-options dropdown
 for (let i = 1; i <= 3; i++) {
     let exerciseEl = `
-        <div id="exercise-${i}" class="exercise">
+        <div id="exercise-${i}" class="exercise" draggable="true">
             <button class="move-exercise"><i class="fa-solid fa-up-down"></i></button>
             <span>Exercise ${i}</span>
             <div class="options">
@@ -30,6 +30,7 @@ for (let i = 1; i <= 3; i++) {
                 </ul>
             </div>
         </div>
+        <div class="drop-zone"></div>
     `
     exercisesList.insertAdjacentHTML("beforeend", exerciseEl)
 
@@ -50,4 +51,60 @@ for (let i = 1; i <= 3; i++) {
 
         optionsMenu.classList.toggle("open")
     })
+
+}
+
+for (const exerciseEl of document.getElementsByClassName("exercise")) {
+    exerciseEl.addEventListener("dragstart", startDrag)
+}
+
+const dropZones = document.getElementsByClassName("drop-zone")
+
+for (const dropZone of dropZones) {
+    dropZone.addEventListener("dragenter", dragEnter)
+    dropZone.addEventListener("dragover", dragOver)
+    dropZone.addEventListener("dragleave", dragLeave)
+    dropZone.addEventListener("drop", drop)
+}
+
+// on the start of dragging the exercise
+function startDrag(e) {
+    e.dataTransfer.setData("text/plain", e.target.id)
+    // hide the exercise in the DOM while dragging it
+    setTimeout(function() {
+        e.target.classList.add("hide")
+    }, 0)
+}
+
+// when draggable exercise enters droppable space (i.e. the space between exercises)
+function dragEnter(e) {
+    e.preventDefault() // allows one exercise to be dropped on top of another one
+    console.log(e.target)
+    e.target.classList.add("drag-over")
+}
+
+// when draggable exercise is inside droppable space (i.e. the space between exercises)
+function dragOver(e) {
+    e.preventDefault() // allows one exercise to be dropped on top of another one
+    e.target.classList.add("drag-over")
+}
+
+// when draggable exercise leaves droppable space
+function dragLeave(e) {
+    e.target.classList.remove("drag-over")
+}
+
+// when draggable exercise is dropped
+function drop(e) {
+    e.target.classList.remove("drag-over")
+
+    // get the dragging exericse
+    const id = e.dataTransfer.getData("text/plain")
+    const dragging = document.getElementById(id)
+
+    // add it to the drop target (i.e. after the drop-zone hovering over)
+    e.target.insertAdjacentElement("afterend", dragging)
+
+    // make the dragging exercise visible again
+    dragging.classList.remove("hide")
 }
