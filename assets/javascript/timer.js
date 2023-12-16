4994e80fd4dbb28db2b37849d11d9e83818c4e5c
Picasso <urvishp13@gmail.com>
2023-12-15 20:03:39 -0500

    added increase/decrease time functionality

--- assets/javascript/timer.js
+++ assets/javascript/timer.js
@@ -1,16 +1,17 @@
 const timer = document.getElementById("timer")
+const increase = document.getElementById("increase")
+const decrease = document.getElementById("decrease")
 
 // get the current time from the DOM
-let time = timer.textContent
+let time = Number(timer.textContent)
 // start the timer
 setInterval(function() {
     // decrease the time by 1 second
     time--
 
     // once time expires,
-    if (time === 0) {
+    if (time <= 0) {
         // redirect the user back to the workout page
-        console.log(time)
         window.location.replace("workout.html")
     }
 
@@ -18,3 +19,15 @@ setInterval(function() {
     timer.textContent = time
 
 }, 1000)
+
+// if the user wants to increase the amount of rest time
+increase.addEventListener("click", function() {
+    // increase the rest time by 10s
+    time += 10
+})
+
+// if the user wants to decrease the amount of rest time
+decrease.addEventListener("click", function() {
+    // decrease the rest time by 10s
+    time -= 10
+})
