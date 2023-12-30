9b8ce68e7b9e5bdfb9e08992625ad779d2068e60
Picasso <urvishp13@gmail.com>
2023-12-29 20:31:12 -0500

    got summary with supersets rendered

--- assets/javascript/end-workout.js
+++ assets/javascript/end-workout.js
@@ -15,9 +15,7 @@ if (workoutType === "supersets") {
     // there will only be 3 sets
     for (let i = 1; i <= 3; i++) {
         summaryHTML += `
-            <div class="set" data-set="${i}">
-                <h4>Set ${i}</h4>
-            </div>
+            <div class="set" data-set="${i}">Set ${i}</div>
         ` 
     }
 }
@@ -32,25 +30,48 @@ else {
 }
 summary.innerHTML = summaryHTML
 
-// FOR SUPERSETS
-// go through the repsCompleted array 1-by-1
-// each set starts at (multiples of numbers of exercises) - 1
-
-// FOR STRAIGHT SETS
-// each set will have 3 instances of 1 exercise
-// each set starts at (multiples of 3) - 1
-
 document.addEventListener("click", function(e) {
     
     if (e.target.dataset.set) {
         // get the set number
         const setNumber = Number(e.target.dataset.set)
-        // console.log(setNumber)
-        // get the set from the repsDone array
-        const setReps = repsCompleted.slice((setNumber - 1) * 3 , setNumber * 3)
-        // console.log(setReps)
 
+        // FOR SUPERSETS
+        // go through the repsCompleted array 1-by-1
+        // each set starts at (multiples of numbers of exercises)
+        if (workoutType === "supersets") {
+            // get the set from the repsDone array
+            const setReps = repsCompleted.slice((setNumber - 1) * (regiment.length), setNumber * (regiment.length)) 
+            
+            let exercisesHTML = ""
+    
+            setReps.forEach((rep, index) => {
+                exercisesHTML += `
+                    <li class="exercise">
+                        <label>${regiment[index].name}</label>
+                        <input type="number" value=${rep} disabled> reps
+                    </li>
+                `  
+            })
+
+            modal.innerHTML = `
+                <button id="close-modal-btn" class="close-modal-btn">Close</button>
+                <h4 class="set-title">Set ${setNumber}</h4>
+                <div class="set-summary">
+                    <ul class="set-exercises">
+                        ${exercisesHTML}
+                    </ul>
+                </div>
+            `
+        }
+
+        // FOR STRAIGHT SETS
+        // each set will have 3 instances of 1 exercise
+        // each set starts at (multiples of 3)
         if (workoutType === "straight sets") {
+            // get the set from the repsDone array
+            const setReps = repsCompleted.slice((setNumber - 1) * 3 , setNumber * 3)  
+
             modal.innerHTML = `
                 <button id="close-modal-btn" class="close-modal-btn">Close</button>
                 <h4 class="set-title">Set ${setNumber}</h4>
@@ -71,10 +92,11 @@ document.addEventListener("click", function(e) {
                     </ul>
                 </div>
             `
-            modal.showModal()
-            document.getElementById("close-modal-btn").addEventListener("click", function() {
-                modal.close()
-            })
         }
+
+        modal.showModal()
+        document.getElementById("close-modal-btn").addEventListener("click", function() {
+            modal.close()
+        })
     }
 })
