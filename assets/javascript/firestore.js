2848dbc63cb1b57424df89eefa71d6b559f15141
Picasso <urvishp13@gmail.com>
2024-04-05 16:17:17 -0400

    added Firebase to project

---
+++ assets/javascript/firestore.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgvoylZI3oit_vH7b_bgcRYcoA3kwoe5A",
+  authDomain: "road-to-gymnastics.firebaseapp.com",
+  projectId: "road-to-gymnastics",
+  storageBucket: "road-to-gymnastics.appspot.com",
+  messagingSenderId: "278259055872",
+  appId: "1:278259055872:web:6754c5c3dc81d89b4cc7e5"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+const exerciseTitle = document.getElementById("new-exercise-title")
+const exerciseDesc = document.getElementById("new-exercise-desc")
+const saveExerciseBtn = document.getElementById("save-btn")
+
+saveExerciseBtn.addEventListener("click", writeExerciseToPage)
+
+function writeExerciseToPage() {
+  exerciseTitle = exerciseTitle.value
+}
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  // The entry point file described above
+  entry: './assets/javascript/firestore.js',
+  // The location of the build folder described above
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true
+  },
+  watch: true,
+  // Optional and for development only. This provides the ability to
+  // map the built code back to the original source format when debugging.
+  devtool: 'eval-source-map',
+};
